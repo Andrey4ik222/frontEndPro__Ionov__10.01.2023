89d2ed6eaa30ab481358671b52c9f5f0b16ee65b
Andrey4ik222 <107884250+Andrey4ik222@users.noreply.github.com>
2023-01-20 10:55:56 +0100

    исправил hw 8

---
+++ hw6/main.js
@@ -0,0 +1,5 @@
+const a = prompt("ffffff");
+if (a == "a") {
+  const b = "c";
+}
+alert(b);
--- hw8/main.js
+++ hw8/main.js
@@ -5,7 +5,7 @@ switch (yearBirth) {
     alert("Шкода, що Ви не захотіли ввести свій рік народження");
     break;
 }
-const cityLive = prompt("Введіть ваше місто проживання:");
+let cityLive = prompt("Введіть ваше місто проживання:");
 switch (cityLive) {
   case null:
     alert("Шкода, що Ви не захотіли ввести своє місто проживання");
@@ -16,50 +16,101 @@ switch (sportFavorite) {
     alert("Шкода, що Ви не захотіли ввести свій улюбленний спорт");
 }
 const age = 2023 - yearBirth;
+let football;
+let hockey;
+let otherSport;
 
-switch (cityLive) {
-  case "Київ":
-    alert(`Ти живеш у столиці України, твій вік: ${age}`);
-    break;
-  case "Варшава":
-    alert(`Ти живеш у столиці Польщі, твій вік: ${age}`);
-    break;
-  case "Лондон":
-    alert(`Ти живеш у столиці Великобританії, твій вік: ${age}`);
-    break;
-  case "Берлін":
-    alert(`Ти живеш у столиці Німеччини, твій вік: ${age}`);
-    break;
-  case "Відень":
-    alert(`Ти живеш у столиці Австрії, твій вік: ${age}`);
-    break;
-  case "Мадрід":
-    alert(`Ти живеш у столиці Іспанії, твій вік: ${age}`);
-    break;
-  case "Лісабон":
-    alert(`Ти живеш у столиці Португалії, твій вік: ${age}`);
-    break;
-  case "Анкара":
-    alert(`Ти живеш у столиці Туреччини, твій вік: ${age}`);
-    break;
-  case "Рим":
-    alert(`Ти живеш у столиці Італії, твій вік: ${age}`);
-    break;
-  case "Париж":
-    alert(`Ти живеш у столиці Франції, твій вік: ${age}`);
-    break;
-  case "Вашингтон":
-    alert(`Ти живеш у столиці Сполучених Штатів Америки, твій вік: ${age}`);
-    break;
-  default:
-    alert(`Ти живеш у місті: ${cityLive}, твій вік: ${age}`);
-}
 if (sportFavorite == "Футбол") {
-  alert("Круто! Хочеш стати Ліонелем Месі?");
+  football = "Круто! Хочеш стати Ліонелем Месі?";
 } else if (sportFavorite == "Хокей") {
-  alert("Круто! Хочеш стати Віктором Тихоновим?");
+  hockey = "Круто! Хочеш стати Віктором Тихоновим?";
 } else if (sportFavorite == "Баскетбол") {
   alert("Круто! Хочеш стати Майклом Джорданом?");
 } else {
-  alert(`Круто! Мені теж подобается ${sportFavorite}`);
+  otherSport = `Круто! Мені теж подобается ${sportFavorite}`;
+}
+switch (cityLive) {
+  case "Київ":
+    alert(
+      `Ти живеш у столиці України, твій вік: ${age}, ${
+        football || hockey || otherSport
+      }`
+    );
+    break;
+  case "Варшава":
+    alert(
+      `Ти живеш у столиці Польщі, твій вік: ${age}, ${
+        football || hockey || otherSport
+      }`
+    );
+    break;
+  case "Лондон":
+    alert(
+      `Ти живеш у столиці Великобританії, твій вік: ${age}, ${
+        football || hockey || otherSport
+      }`
+    );
+    break;
+  case "Берлін":
+    alert(
+      `Ти живеш у столиці Німеччини, твій вік: ${age}, ${
+        football || hockey || otherSport
+      }`
+    );
+    break;
+  case "Відень":
+    alert(
+      `Ти живеш у столиці Австрії, твій вік: ${age}, ${
+        football || hockey || otherSport
+      }`
+    );
+    break;
+  case "Мадрід":
+    alert(
+      `Ти живеш у столиці Іспанії, твій вік: ${age}, ${
+        football || hockey || otherSport
+      }`
+    );
+    break;
+  case "Лісабон":
+    alert(
+      `Ти живеш у столиці Португалії, твій вік: ${age}, ${
+        football || hockey || otherSport
+      }`
+    );
+    break;
+  case "Анкара":
+    alert(
+      `Ти живеш у столиці Туреччини, твій вік: ${age}, ${
+        football || hockey || otherSport
+      }`
+    );
+    break;
+  case "Рим":
+    alert(
+      `Ти живеш у столиці Італії, твій вік: ${age}, ${
+        football || hockey || otherSport
+      }`
+    );
+    break;
+  case "Париж":
+    alert(
+      `Ти живеш у столиці Франції, твій вік: ${age}, ${
+        football || hockey || otherSport
+      }`
+    );
+    break;
+  case "Вашингтон":
+    alert(
+      `Ти живеш у столиці Сполучених Штатів Америки, твій вік: ${age}, ${
+        football || hockey || otherSport
+      }`
+    );
+    break;
+  default:
+    alert(
+      `Ти живеш у місті: ${cityLive}, твій вік: ${age}, ${
+        football || hockey || otherSport
+      }`
+    );
 }
