e89c300fd8a629a8663942046d5b951f29d1b9f0
Andrey4ik222 <107884250+Andrey4ik222@users.noreply.github.com>
2023-01-20 11:30:45 +0100

    hw6

--- hw6/main.js
+++ hw6/main.js
@@ -1,5 +1,14 @@
-const a = prompt("ffffff");
-if (a == "a") {
-  const b = "c";
+const firstLine = prompt("Перший рядок");
+const secondLine = prompt("Другий рядок:");
+const thirdLine = prompt("Третій рядок:");
+const mult = firstLine + secondLine + thirdLine;
+alert(mult);
+
+const num = prompt("Введіть п'ятизначне число:");
+if (num.length != 5) {
+  alert("Введить п'ятизначне число");
+} else {
+  alert(
+    `${num[0] + " " + num[1] + " " + num[2] + " " + num[3] + " " + num[4]}`
+  );
 }
-alert(b);
