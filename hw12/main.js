5a01e2d10fc2ffbf993de2b2dd9fbb6420991379
Andrey4ik222 <107884250+Andrey4ik222@users.noreply.github.com>
2023-01-25 16:00:23 +0100

    изменил удаленные элементы

--- hw12/main.js
+++ hw12/main.js
@@ -1,5 +1,5 @@
 const numbersArray = [2, 34, 1, 8, 3, 5, 4];
 const newNumbersArray = numbersArray.sort((a, b) => a - b);
 console.log(newNumbersArray);
-newNumbersArray.splice(2, 3);
+newNumbersArray.splice(1, 3);
 console.log(newNumbersArray);
