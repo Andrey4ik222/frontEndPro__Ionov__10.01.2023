f0a015668c1e9987564b43a486d0eebe9973a0af
Andrey4ik222 <107884250+Andrey4ik222@users.noreply.github.com>
2023-02-09 12:31:52 +0100

    hw20

---
+++ hw20/main.js
@@ -0,0 +1,10 @@
+function exponentiation(num, degree) {
+  if (degree === 1) {
+    return num;
+  } else {
+    return num * exponentiation(num, degree - 1);
+  }
+}
+const number = +prompt("Number:");
+const degree = +prompt("degree:");
+console.log(exponentiation(number, degree));
