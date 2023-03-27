a2fefdee7613a7b95d1b67c928fbe9ae1c90914b
Andrey4ik222 <107884250+Andrey4ik222@users.noreply.github.com>
2023-03-27 13:53:11 +0200

    hw38

---
+++ hw38/script.js
@@ -0,0 +1,36 @@
+// Пишемо свій слайдер зображень.
+
+// Відображаємо зображення та кнопки Next, Prev з боків від зображення.
+// При кліку на Next - показуємо наступне зображення.
+// При кліку на Prev - попереднє .
+// При досягненні останнього зображення - ховати кнопку Next. Аналогічно з першим зображенням і кнопкою Prev.
+
+const btnNext = document.querySelector(".btn-next");
+const btnPrev = document.querySelector(".btn-prev");
+
+let imageIndex = 1;
+
+function nextImage() {
+  showImages((imageIndex += 1));
+}
+function previousImage() {
+  showImages((imageIndex -= 1));
+}
+function showImages(index) {
+  const images = document.getElementsByClassName("img");
+  if (index === 9) {
+    btnNext.hidden = true;
+  } else if (index === 1) {
+    btnPrev.hidden = true;
+  } else {
+    btnNext.hidden = false;
+    btnPrev.hidden = false;
+  }
+  for (let img of images) {
+    img.style.display = "none";
+  }
+  images[imageIndex - 1].style.display = "block";
+}
+showImages(imageIndex);
+btnNext.addEventListener("click", nextImage);
+btnPrev.addEventListener("click", previousImage);
